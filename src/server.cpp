3efddee3ef979a2be5417d82e5fc7a67fb80b2b2
yanruijie902136 <yanruijie902136@gmail.com>
2024-07-24 01:32:59 +0800

    pass 1st stage

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <set>
 #include <string>
 #include <cstring>
 #include <unistd.h>
@@ -7,6 +8,7 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <sys/select.h>
 
 #include "HttpRequest.hpp"
 #include "HttpResponse.hpp"
@@ -52,38 +54,55 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
+    fd_set readfds, readfds_copy;
+    FD_ZERO(&readfds);
+    FD_SET(server_fd, &readfds);
+    std::set<int> client_fds;
 
     std::cout << "Waiting for a client to connect...\n";
 
-    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-    std::cout << "Client connected\n";
+    for ( ; ; ) {
+        std::memcpy(&readfds_copy, &readfds, sizeof(readfds));
+        select(FD_SETSIZE, &readfds_copy, nullptr, nullptr, nullptr);
 
-    HttpRequest request = HttpRequest::read_from_socket(client_fd);
-    // std::cout << request << "\n";
+        if (FD_ISSET(server_fd, &readfds_copy)) {
+            struct sockaddr_in client_addr;
+            socklen_t client_addr_len = sizeof(client_addr);
+            int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+            std::cout << "Client connected\n";
+            FD_SET(client_fd, &readfds);
+            client_fds.insert(client_fd);
+        }
 
-    HttpResponse response;
-    if (request.target == "/user-agent") {
-        response.status = HttpStatus::OK;
-        response.body = request.headers["User-Agent"];
-        response.headers["Content-Type"] = "text/plain";
-        response.headers["Content-Length"] = std::to_string(response.body.length());
+        for (auto it = client_fds.begin(); it != client_fds.end(); ) {
+            int client_fd = *it;
+            if (!FD_ISSET(client_fd, &readfds_copy)) {
+                it++;
+                continue;
+            }
+
+            HttpRequest request = HttpRequest::read_from_socket(client_fd);
+            // std::cout << request << "\n";
+
+            HttpResponse response;
+            if (request.target == "/user-agent") {
+                response.status = HttpStatus::OK;
+                response.body = request.headers["User-Agent"];
+                response.headers["Content-Type"] = "text/plain";
+                response.headers["Content-Length"] = std::to_string(response.body.length());
+            }
+            else if (request.target.starts_with("/echo/")) {
+                response.status = HttpStatus::OK;
+                response.body = request.target.substr(6);
+                response.headers["Content-Type"] = "text/plain";
+                response.headers["Content-Length"] = std::to_string(response.body.length());
+            }
+            else {
+                response.status = request.target == "/" ? HttpStatus::OK : HttpStatus::NOT_FOUND;
+            }
+
+            std::string response_str = response.str();
+            send(client_fd, response_str.c_str(), response_str.length(), 0);
+        }
     }
-    else if (request.target.starts_with("/echo/")) {
-        response.status = HttpStatus::OK;
-        response.body = request.target.substr(6);
-        response.headers["Content-Type"] = "text/plain";
-        response.headers["Content-Length"] = std::to_string(response.body.length());
-    }
-    else {
-        response.status = request.target == "/" ? HttpStatus::OK : HttpStatus::NOT_FOUND;
-    }
-
-    std::string response_str = response.str();
-    send(client_fd, response_str.c_str(), response_str.length(), 0);
-
-    close(server_fd);
-
-    return 0;
 }
